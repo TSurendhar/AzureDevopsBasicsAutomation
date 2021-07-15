83725475ac9374103ded406ec50bfd6fe9321d49
Surendhar Thallapelly <st653595@broadcom.net>
2021-07-15 15:06:42 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -8,28 +8,30 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.chrome.ChromeDriverService;
 
 public class BrowserTest {
 
-	
 	@Test
-	public void getData()
-	{
+	public void getData() {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
+		ChromeDriverService chromeDriverService = ChromeDriverService.createDefaultService();
+		WebDriver driver = new ChromeDriver(chromeDriverService);
 		driver.get("https://surendharthallapelly.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
+		String text = driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
+
 		Assert.assertTrue(text.equalsIgnoreCase("SurendharThallapelly Azure Devops Learning"));
-		try
-		{
-		  Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
-		}
-		
-		catch(Exception e) {}
 		driver.close();
+
+		try {
+			driver.quit();
+			Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe /T");
+		}
+
+		catch (Exception e) {
+		}
+
 	}
 }
